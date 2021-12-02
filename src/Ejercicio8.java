a31396ab7e41d5ea7d271cc2564fbdc35b930bf7
Eva Ramos <eva.ramos@iesnervion.es>
2021-12-02 12:41:24 +0100

    version factoria 2

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class Ejercicio8 {
+
+    static Scanner sc=new Scanner(System.in);
+
     public static void main(String[] args) {
         /*8. Diseñar un subprograma para pintar la pirámide de Tartaglia (Busca en Internet las
         especificaciones).*/
 
-
+        long filas;
+        System.out.println("introduzca la altura de la piramide");
+        filas=sc.nextLong();
+        pintarPiramide(filas);
 
     }
-    public static long hallarFactorial(int num) {
 
+    public static int pedirDatos(){
+
+        System.out.println("Introduzca el valor que desee");
+        return sc.nextInt();
+    }
+    public static long hallarFactorial(long num) {
+
+        num=pedirDatos();
         long factorial = 1;
         for (long i = 1; i <= num; i++) {
             factorial = factorial * i;
@@ -15,25 +30,28 @@ public class Ejercicio8 {
         return factorial;
     }
 
-    public static long hallarCombinatorio(int n, int m) {
+    public static long hallarCombinatorio(long n, long m) {
 
-        long factn = hallarFactorial(n);
-        long factm = hallarFactorial(m);
-        long restaFact = hallarFactorial(n - m);
-        long combinatorio = factn / (factm * restaFact);
+         n = hallarFactorial(pedirDatos());
+         m = hallarFactorial(pedirDatos());
+        long restaFact = hallarFactorial(n-m);
 
-        return combinatorio;
+
+        return n / (m * restaFact);
     }
 
     public static void pintarPiramide(long altura){
 
+
         for (long i=0;i<=altura-1;i++){
             for (int k=0;k<=altura;k++){
-                System.out.print(" ");
+                System.out.print(k+" ");
             }
             for (long j=0;j<=i;i++){
                 System.out.print("");
+                System.out.print(hallarCombinatorio(i,j)+" ");
             }
+
         }
 
     }
